6957d022b78da7b3e81ef6c5b301ee28b30e58b4
Daniel Rodenstein <daniel.rodenstein@tu-clausthal.de>
2023-07-08 12:00:54 +0200

    Pins

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #Hackathon 2023
+import RPi.GPIO as GPIO
+
+
+#Pin Belegung
+
+#Reed
+reed_a = 22
+reed_b = 23
+reed_c = 24
+reed_d = 25
+
+# Led Strasse
+strassen_leds = 20
+#Led Zug
+bahn_led_a = 4
+bahn_led_b = 16
+
+#Servo
