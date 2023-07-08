5937764d7ba73c695a15abc25dfc29274a46e5d1
c8p2d01 <c8p2d01@gmail.com>
2023-07-08 12:17:24 +0200

    reed pin 22

--- test_reed.py
+++ test_reed.py
@@ -3,8 +3,8 @@
 import time, sys
 import RPi. GPIO as GPIO
 GPIO.setmode (GPIO. BOARD)
-GPIO setup (15, GPIO .IN, pull_up_down = GPIO.PUD_UP)
+GPIO setup (22, GPIO .IN, pull_up_down = GPIO.PUD_UP)
 while True:
-input = GPIO.input (15)
+input = GPIO.input (22)
 print (input)
 time.sleep (1)
