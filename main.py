1ed14668880f7aa6017f070a6894259092a37e74
c8p2d01 <c8p2d01@gmail.com>
2023-07-08 14:55:20 +0200

    lichter und schranken

--- main.py
+++ main.py
@@ -17,19 +17,18 @@ reed_rechts_aussen_pin = 25
 bahn_led_links = LED(4)
 bahn_led_rechts = LED(16)
 
-def schliesse_Uebergang(seite):
-	#Schranke mach zu
+# Led Strasse
+strassen_leds = LED(20)
+
+def bahnlicht_freigabe(seite):
 	if (seite == "links"):
 		bahn_led_links.blink(1, 1)
 	else:
 		bahn_led_rechts.blink(1, 1)
 
-def oeffne_Uebergang(seite):
-	#Schranke mach auf
-	if (seite == "links"):
-		bahn_led_links.off()
-	else:
-		bahn_led_rechts.off()
+def bahnlicht_ende(seite):
+	bahn_led_links.off()
+	bahn_led_rechts.off()
 
 class	bahn_sensor:
 	def __init__(self, seite):
@@ -47,9 +46,9 @@ class	bahn_sensor:
 			self.status_innen = 0
 		if (self.status_aussen == 1 and self.status_innen == 1):
 			if (self.activation_aussen > self.activation_innen):
-				schliesse_Uebergang(self.seite)
+				bahnlicht_freigabe(self.name)
 			else:
-				oeffne_Uebergang(self.seite)
+				bahnlicht_ende(self.name)
 	
 	def activate_aussen(self):
 		self.status_aussen = 1
@@ -61,14 +60,34 @@ class	bahn_sensor:
 		self.activate_innen = time.time()
 		self.checkTravel()
 
-bahnSensorLinks = bahn_sensor(links)
-bahnSensorRechts = bahn_sensor(rechts)
+bahnSensorLinks = bahn_sensor("links")
+bahnSensorRechts = bahn_sensor("rechts")
 
+# False -> kein Zug im Bahnübergang, True -> Bahn im Zugübergang
 innen:bool = False
 aussen:bool = False
 
-# Led Strasse
-strassen_leds = LED(20)
+def	strassenlicht_switch():
+	if (aussen):
+		strassen_leds.off()
+		aussen = False
+	else:
+		strassen_leds.on()
+		aussen = True
+
+def	strassenschranken_switch():
+	if (innen):
+		#schranke auf
+		innen = False
+	else:
+		#schranke zu
+		innen = True
+
+def	Fehlerzustand():
+	strassen_leds.on
+	bahn_led_links.off()
+	bahn_led_rechts.off()
+	#Schranken zu?
 
 #Servo
 
@@ -80,12 +99,16 @@ GPIO.setup(reed_rechts_aussen_pin, GPIO.IN)
 
 def callback_function1():
 	bahnSensorLinks.activate_aussen()
+	strassenlicht_switch()
 def callback_function2():
 	bahnSensorLinks.activate_innen()
+	strassenschranken_switch()
 def callback_function3():
 	bahnSensorRechts.activate_aussen()
+	strassenlicht_switch()
 def callback_function4():
 	bahnSensorRechts.activate_innen()
+	strassenschranken_switch()
 
 GPIO.add_event_detect(reed_links_aussen_pin, GPIO.FALLING, callback=callback_function1, bouncetime=200)
 GPIO.add_event_detect(reed_links_innen_pin, GPIO.FALLING, callback=callback_function2, bouncetime=200)
@@ -94,23 +117,22 @@ GPIO.add_event_detect(reed_rechts_aussen_pin, GPIO.FALLING, callback=callback_fu
 
 #Bahnuebergang sichern
 # Straßen-LED einschalten
-strassen_leds.on()
 
 #wenn der Bahnübergang gesichert ist, dann die richtige Richtung mit Blinklicht freigeben
 
-if richtung == links:
-# Bahn-LED im Verhältnis 1 Sekunde zu 1 Sekunden blinken lassen
-bahn_led_links.blink(1,1)
-elif richtung == rechts:
-# Bahn-LED im Verhältnis 1 Sekunde zu 1 Sekunden blinken lassen
-bahn_led_rechts.blink(1,1)
+# #if richtung == links:
+# # Bahn-LED im Verhältnis 1 Sekunde zu 1 Sekunden blinken lassen
+# bahn_led_links.blink(1,1)
+# elif richtung == rechts:
+# # Bahn-LED im Verhältnis 1 Sekunde zu 1 Sekunden blinken lassen
+# bahn_led_rechts.blink(1,1)
 
 # Signal geben wenn der Bahnübergang frei ist
 
-#Bahnuebergang freischalten, wenn der Bahnuebergang frei ist
-# Straßen-LED ausschalten
-strassen_leds.off()
-# Bahn-LEDs ausschalten
-bahn_led_rechts.off()
-bahn_led_links.off()
+# #Bahnuebergang freischalten, wenn der Bahnuebergang frei ist
+# # Straßen-LED ausschalten
+# strassen_leds.off()
+# # Bahn-LEDs ausschalten
+# bahn_led_rechts.off()
+# bahn_led_links.off()
 
